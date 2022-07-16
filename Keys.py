a29d5f34e26fd59d478ba94146bfe353ddb7538e
Muhammed Fatih Çolak <95385285+mfatihcolak@users.noreply.github.com>
2022-07-16 22:49:24 +0300

    Update Keys.py

--- Keys.py
+++ Keys.py
@@ -1,6 +1,6 @@
-apiKey = "CnLNarMnazevhIWwWWbEFLdgo75xUln5KIT7GFQPBBjhJDesMbHixBm7rpTV262z"
-secretKey = "iCpCGbuw5wtPg3xnpyBA01khz6XvBI03CCWqcrDnXd0QwEg0oQz2NshaQtjoS5z1"
+apiKey = ""
+secretKey = ""
 
-telegramToken= "5277890297:AAGthoddwfdibH1ELNQMuMg7qasLVvoKtNg"
-telegramId = "908516722"
-telegramGroupId = "-683752848"
+telegramToken= ""
+telegramId = ""
+telegramGroupId = ""
